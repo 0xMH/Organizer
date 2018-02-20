67b76c92e030e0b44702288412c6889b87f8fe3a
NoN <hamo220058@gmail.com>
2018-02-20 22:23:35 +0200

    Uploading...

---
+++ Organizer.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+ 
+DIRECTORIES = {
+    "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
+               ".heif", ".psd"],
+    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+               ".qt", ".mpg", ".mpeg", ".3gp", ".mkv"],
+    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
+                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
+                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
+                  "pptx", ".odp"],
+    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
+                 ".dmg", ".rar", ".xar", ".zip"],
+    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+              ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
+    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PDF": [".pdf"],
+    "PYTHON": [".py"],
+    "XML": [".xml"],
+    "EXE": [".exe"],
+    "SHELL": [".sh"],
+    "OPENVPN": [".ovpn"],
+    "TORRENT": [".torrent"],
+    "TRANSLATION": [".srt"],
+    "SQL": [".sql"]
+ 
+}
+ 
+FILE_FORMATS = {file_format: directory
+                for directory, file_formats in DIRECTORIES.items()
+                for file_format in file_formats}
+MyName = os.path.basename(__file__) 
+def organize_junk():
+    for entry in os.scandir():
+        if entry.is_dir() or entry.name == MyName:
+            continue
+        file_path = Path(entry)
+        file_format = file_path.suffix.lower()
+        if file_format in FILE_FORMATS:
+            directory_path = Path(FILE_FORMATS[file_format])
+            directory_path.mkdir(exist_ok=True)
+            file_path.rename(directory_path.joinpath(file_path))
+ 
+        for dir in os.scandir():
+            try:
+                os.rmdir(dir)
+            except:
+                pass
+ 
+if __name__ == "__main__":
+    organize_junk()
